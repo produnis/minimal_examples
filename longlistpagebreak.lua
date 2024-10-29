74e2b9e3ef39db2afb0e1edea6de9360d06f56ab
Joe Slam <schlarmann@produnis.de>
2024-10-29 20:05:27 +0100

    does not work

--- longlistpagebreak.lua
+++ longlistpagebreak.lua
@@ -9,8 +9,8 @@ function Div(el)
         RawBlock = function(raw_el)
           if raw_el.format == 'latex' then
             -- More robust regular expressions to handle whitespace and potential nesting
-            local start_pattern = "\\s*\\begin{minipage}[t]{\\textwidth - 5.5mm}\\s*"
-            local end_pattern = "\\s*\\end{minipage}\\s*"
+            local start_pattern = "\\begin{minipage}[t]{\\textwidth - 5.5mm}"
+            local end_pattern = "\\end{minipage}"
             raw_el.text = raw_el.text:gsub(start_pattern, "")
             raw_el.text = raw_el.text:gsub(end_pattern, "")
           end
