e34cda835e35b1754c45571721014f8394264a80
Joe Slam <schlarmann@produnis.de>
2024-10-31 15:03:18 +0100

    even more new approach

--- longlistpagebreak.lua
+++ longlistpagebreak.lua
@@ -1,32 +1,21 @@
--- Lua writer to remove specific minipage environments in marked sections for Quarto/Pandoc
+-- Lua Filter for Quarto: Insert specific HTML for longlistcallout sections
 
-local start_marker = "%% THIS IS THE START OF MY INDICATOR"
-local end_marker = "%% HERE IT ENDS"
+function Div(el)
+  if el.classes:includes("longlistcallout") then
+    if quarto.doc.is_format("latex") then
+      -- LaTeX format: Add start and end code for longlistcallout
+      table.insert(el.content, 1, pandoc.RawBlock("latex", [[
+\begin{tcolorbox}[enhanced jigsaw, rightrule=.15mm, breakable, arc=.35mm, leftrule=.75mm, bottomrule=.15mm, colframe=quarto-callout-note-color-frame, colback=white, opacityback=0, left=2mm, toprule=.15mm]
+\begin{minipage}[t]{5.5mm}
+\textcolor{quarto-callout-note-color}{\faInfo}
+\end{minipage}%
+      ]]))
+      table.insert(el.content, pandoc.RawBlock("latex", [[
+\end{tcolorbox}
+      ]]))
 
-function Writer(doc, output)
-    local output = pandoc.write(doc, "latex")
-
-    local in_filtered_section = false
-    local begin_minipage_pattern = "\\begin%{minipage%}%[t%]%{[^}]+%}"
-    local end_minipage_pattern = "\\end%{minipage%}"
-
-    local processed_output = {}
-
-    for line in output:gmatch("[^\r\n]+") do
-        if line:match(start_marker) then
-            in_filtered_section = true
-        elseif line:match(end_marker) then
-            in_filtered_section = false
-        end
-
-        if in_filtered_section then
-            if not line:match(begin_minipage_pattern) and not line:match(end_minipage_pattern) then
-                table.insert(processed_output, line)
-            end
-        else
-            table.insert(processed_output, line)
-        end
     end
-
-    return table.concat(processed_output, "\n")
+    -- Return modified element to apply changes
+    return el
+  end
 end
